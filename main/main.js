08957dbf83e24d41e5eb9f6ccbd17f7affa85bfb
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-06 23:15:48 +0800

    Could return game result

--- main/main.js
+++ main/main.js
@@ -1,6 +1,21 @@
-// Write your cade below:
-function main() {};
+const generateAnswer = require('./AnswerGenerator');
+const compare = require('./Comparetor');
+const parse = require('./Parser');
+
+function Game(answerGenerator) {
+    return {
+        answer : answerGenerator ? answerGenerator.generateAnswer() : generateAnswer(),
+        check: function (input) {
+            let compareResult = compare(this.answer, parse(input));
+            return formatResult(compareResult);
+        }
+    }
+};
+
+function formatResult(result) {
+    return `${result.correct}A${result.onlyInclude}B`;
+}
 
 module.exports = {
-    main
+    Game
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,15 @@
-const main = require('../main/main');
+const {Game} = require('../main/main');
+const generateAnswer = require('../main/AnswerGenerator');
 
 describe('main()', () => {
 
+    it('should return 2A1B when the 3 number is correct', () => {
+        const input = '1 2 5 3';
+
+        const game = new Game({generateAnswer: () => [1,2,3,4]});
+        const result = game.check(input);
+
+        const expected = '2A1B';
+        expect(result).toEqual(expected);
+    });
 });
