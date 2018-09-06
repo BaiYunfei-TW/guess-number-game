4908303f028c6517122eaa2157d291dca2a6b1ea
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-06 22:43:47 +0800

    Could return compare result when 3 number of input is correct

--- spec/Comparetor-spec.js
+++ spec/Comparetor-spec.js
@@ -1,10 +1,17 @@
 const compare = require('../main/Comparetor');
 
 describe('test compare answer with input', () => {
-    it('should return 4A0B when the input in corret', () => {
+    it('should return 4A0B when the input is correct', () => {
         const answer = [1, 2, 3, 4];
         const input = [1, 2, 3, 4];
         const result = compare(answer, input);
         expect(result).toEqual('4A0B');
+    });
+
+    it('should return 3A0B when the 3 number is correct', () => {
+        const answer = [1, 2, 3, 4];
+        const input = [1, 2, 3, 5];
+        const result = compare(answer, input);
+        expect(result).toEqual('3A0B');
     });
 });
